f11546e2c572d703f82e111d882fc0609b03dda6
nikhilbhatia08 <bhatianikhil551@gmail.com>
2023-09-24 01:08:18 +0530

    Initial commit

---
+++ src/test/java/com/example/omnifybackend/OmnifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.omnifybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnifyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
