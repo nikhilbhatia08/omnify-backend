e019c18cd24f79062f3ab1d500101a62431635b8
nikhilbhatia08 <bhatianikhil551@gmail.com>
2023-09-24 15:00:37 +0530

    change in build.gradle

--- src/main/java/com/example/main/SpringWeatherApp.java
+++ src/main/java/com/example/main/SpringWeatherApp.java
@@ -1,5 +1,6 @@
 package com.example.main;
 
+import com.example.main.controller.WeatherController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
